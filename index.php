11b72f038bdb7519d278a7f12401e9e5856d665c
Maria Youssef <mariayoussef61@gmail.com>
2025-08-31 21:09:46 +0300

    db edit

--- index.php
+++ index.php
@@ -1,11 +1,17 @@
 <?php
-$dsn = "mysql:host=localhost;dbname=web3proj";
-$dbusername = "root";
-$dbpass = "";
-try {
-    $pdo = new PDO($dsn, $dbusername, $dbpass);
+$host = "bwfklj7lgile0zzdf6mq-mysql.services.clever-cloud.com";
+$user = "umwekpqauqifmfeg";
+$pass = "yourPasswordHere";
+$db   = "bwfklj7lgile0zzdf6mq";
+$port = 3306;
 
-    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-} catch (PDOException $e) {
-    echo "Connection failed" . $e->getMessage();
+// Create connection
+$conn = new mysqli($host, $user, $pass, $db, $port);
+
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+} else {
+    echo "✅ Connected successfully to Clever Cloud MySQL!";
 }
+?>
